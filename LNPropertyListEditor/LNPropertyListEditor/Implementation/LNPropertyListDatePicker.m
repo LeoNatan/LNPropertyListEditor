1cd9e5322496bf8bbe8533bce7c8db6042b383bc
Leo Natan <leo.natan@outlook.com>
2021-05-05 17:43:36 +0300

    Remove unnecessary time picker

--- LNPropertyListEditor/LNPropertyListEditor/Implementation/LNPropertyListDatePicker.m
+++ LNPropertyListEditor/LNPropertyListEditor/Implementation/LNPropertyListDatePicker.m
@@ -34,12 +34,11 @@ IB_DESIGNABLE
 @implementation LNPropertyListDatePicker
 {
 	_LNPropertyListDatePicker* _datePicker;
-	_LNPropertyListDatePicker* _timePicker;
 }
 
 - (void)prepareForInterfaceBuilder
 {
-	_timePicker.dateValue = _datePicker.dateValue = [NSDate date];
+	_datePicker.dateValue = [NSDate date];
 }
 
 - (void)setEnabled:(BOOL)enabled
@@ -47,7 +46,6 @@ IB_DESIGNABLE
 	[super setEnabled:enabled];
 	
 	[_datePicker setEnabled:enabled];
-	[_timePicker setEnabled:enabled];
 }
 
 - (void)awakeFromNib
@@ -60,44 +58,26 @@ IB_DESIGNABLE
 	_datePicker.cell = [LNPropertyListDatePickerCell new];
 	_datePicker.font = font;
 	_datePicker.datePickerStyle = NSDatePickerStyleTextField;
-	_datePicker.datePickerElements = NSDatePickerElementFlagYearMonthDay | NSDatePickerElementFlagEra;
+	_datePicker.datePickerElements = NSDatePickerElementFlagYearMonthDay | NSDatePickerElementFlagEra | NSDatePickerElementFlagHourMinuteSecond | NSDatePickerElementFlagTimeZone;
 	_datePicker.bordered = NO;
 	_datePicker.drawsBackground = NO;
 	_datePicker.translatesAutoresizingMaskIntoConstraints = NO;
 	_datePicker.target = self;
 	_datePicker.action = @selector(_internalDatePickerValueChanged:);
 	
-	_timePicker = [_LNPropertyListDatePicker new];
-	LNPropertyListDatePickerCell* timePickerCell = [LNPropertyListDatePickerCell new];
-	_timePicker.cell = timePickerCell;
-	_timePicker.font = font;
-	_timePicker.datePickerStyle = NSDatePickerStyleTextField;
-	_timePicker.datePickerElements = NSDatePickerElementFlagHourMinuteSecond | NSDatePickerElementFlagTimeZone;
-	_timePicker.bordered = NO;
-	_timePicker.drawsBackground = NO;
-	_timePicker.translatesAutoresizingMaskIntoConstraints = NO;
-	_timePicker.target = self;
-	_timePicker.action = @selector(_internalDatePickerValueChanged:);
-	
 	_LNPropertyListDatePickerInnerCell* cell = [_LNPropertyListDatePickerInnerCell new];
-	cell.childCells = @[_datePicker.cell, _timePicker.cell];
+	cell.childCells = @[_datePicker.cell];
 	cell.bordered = NO;
 	self.cell = cell;
 	
 	[self addSubview:_datePicker];
-	[self addSubview:_timePicker];
 	
 	[_datePicker setContentHuggingPriority:NSLayoutPriorityRequired forOrientation:NSLayoutConstraintOrientationHorizontal];
 	[_datePicker setContentCompressionResistancePriority:NSLayoutPriorityRequired forOrientation:NSLayoutConstraintOrientationHorizontal];
-	[_timePicker setContentHuggingPriority:NSLayoutPriorityRequired forOrientation:NSLayoutConstraintOrientationHorizontal];
-	[_timePicker setContentCompressionResistancePriority:NSLayoutPriorityRequired forOrientation:NSLayoutConstraintOrientationHorizontal];
 	[NSLayoutConstraint activateConstraints:@[
 											  [self.heightAnchor constraintEqualToAnchor:_datePicker.heightAnchor],
 											  [self.leadingAnchor constraintEqualToAnchor:_datePicker.leadingAnchor],
 											  [self.centerYAnchor constraintEqualToAnchor:_datePicker.centerYAnchor constant:-1.5],
-											  [_timePicker.leadingAnchor constraintEqualToAnchor:_datePicker.trailingAnchor constant:2],
-											  [self.centerYAnchor constraintEqualToAnchor:_timePicker.centerYAnchor constant:-1.5],
-											  [self.trailingAnchor constraintEqualToAnchor:_timePicker.trailingAnchor],
 											  ]];
 	
 	[self setContentHuggingPriority:NSLayoutPriorityRequired forOrientation:NSLayoutConstraintOrientationHorizontal];
@@ -112,11 +92,8 @@ IB_DESIGNABLE
 	self.objectValue = dateValue;
 	
 	_datePicker.dateValue = self.dateValue;
-	_timePicker.dateValue = self.dateValue;
 	_datePicker.visualDatePicker.dateValue = self.dateValue;
 	_datePicker.textDatePicker.dateValue = self.dateValue;
-	_timePicker.visualDatePicker.dateValue = self.dateValue;
-	_timePicker.textDatePicker.dateValue = self.dateValue;
 	
 	if(sendAction)
 	{
